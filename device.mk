93b360c280751225a32f76bd67678a2d46a7d527
David Wronek <david@mainlining.org>
2025-05-04 08:27:10 +0200

    gts8wifi: Import QCOM common configs
    Co-authored-by: chaptsand <chaptsand@gmail.com>
    Signed-off-by: David Wronek <david@mainlining.org>
    Change-Id: I50a49ad81e87d81fec768028968ff62f10cfd774

--- device.mk
+++ device.mk
@@ -30,6 +30,11 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 # Product characteristics
 PRODUCT_CHARACTERISTICS := tablet
 
+# QTI
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/permissions/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
+    $(LOCAL_PATH)/configs/permissions/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
+
 # Rootdir
 PRODUCT_PACKAGES += \
     hdm_status.sh \
