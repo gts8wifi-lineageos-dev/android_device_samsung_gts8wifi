337d8b9ac67fcc377931540c8b482edd7eea5dfe
TARKZiM <tom8476oo@gmail.com>
2025-09-02 20:16:57 +0800

    gts8wifi: Update soong namespaces

--- device.mk
+++ device.mk
@@ -212,7 +212,10 @@ PRODUCT_COPY_FILES += \
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    hardware/samsung \
+    kernel/samsung/sm8450 \
+    kernel/samsung/sm8450-modules
 
 # Inherit the proprietary files
 $(call inherit-product, vendor/samsung/gts8wifi/gts8wifi-vendor.mk)
