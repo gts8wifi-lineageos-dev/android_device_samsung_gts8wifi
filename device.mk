c3682bafa8c777f095c77c79c1bf72638ac1ce71
TARKZiM <tom8476oo@gmail.com>
2025-09-02 20:20:59 +0800

    gts8wifi: Copy USB permissions

--- device.mk
+++ device.mk
@@ -217,5 +217,10 @@ PRODUCT_SOONG_NAMESPACES += \
     kernel/samsung/sm8450 \
     kernel/samsung/sm8450-modules
 
+# USB
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/samsung/gts8wifi/gts8wifi-vendor.mk)
