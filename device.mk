a36cfef84c3cfd871daab23b6f2ce03face8ca18
TARKZiM <tom8476oo@gmail.com>
2025-09-02 20:12:07 +0800

    gts8wifi: Build samsung health hal

--- device.mk
+++ device.mk
@@ -90,9 +90,8 @@ PRODUCT_COPY_FILES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-impl.recovery \
-    android.hardware.health@2.1-service
+    android.hardware.health-service.samsung \
+    android.hardware.health-service.samsung-recovery
 
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
