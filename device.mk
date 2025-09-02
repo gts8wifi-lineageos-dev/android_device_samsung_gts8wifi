50a69778c02ee22a143ede902ffed5041ff6d09b
TARKZiM <tom8476oo@gmail.com>
2025-09-02 20:14:21 +0800

    gts8wifi: Build Lineage health

--- device.mk
+++ device.mk
@@ -108,6 +108,12 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.keystore.app_attest_key.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.keystore.app_attest_key.xml \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml
 
+# Lineage Health
+PRODUCT_PACKAGES += \
+    vendor.lineage.health-service.default
+
+$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/power_supply/battery/batt_slate_mode)
+
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
